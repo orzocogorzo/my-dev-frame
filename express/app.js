c2b0095fc4abc288e78d63952c23336cebbc1782
Lucas García <lucas@bestiario.org>
2018-09-23 17:56:17 +0200

    update app express

--- express/app.js
+++ express/app.js
@@ -2,13 +2,13 @@ const fs = require('fs');
 const path = require('path');
 
 const webpack = require('webpack');
-
+const bodyParser = require("body-parser")
 const config = require('./webdev.config');
 const envConfig = new Object();
 
 const http = require('http');
 const express = require('express');
-const livereload = require('express-livereload');
+//const livereload = require('express-livereload');
 const reload = require('./liveserver');
 
 
@@ -30,22 +30,27 @@ function flagMatcher(flag){
 }
 
 function registerEnvironment( callback ) {
-  const environments =  require( config.envFile ); //require('../environments.js');
+  const environments = require(config.envFile);
 
-  if ( !environments[ envConfig[ "env" ]] ) {
+  if (!environments[envConfig["env"]]) {
     console.warn("Not recognized environment declaration. Working with default config");
   };
   
-  envConfig[ "env" ] = env = environments[ envConfig[ "env" ]] || { 
+  envConfig["env"] = env = environments[envConfig["env"]] || { 
     name: 'development',
-    apiURL: 'data/json'
+    apiURL: 'rs/json'
   };
 
-  fs.writeFile( path.resolve( config.distDir, 'environment.js' ), ' window.environment='+JSON.stringify( env )+';', function( err ) {
-    if ( err ) {
+  if (!fs.existsSync(config.distDir)) {
+    fs.mkdirSync(config.distDir);
+  };
+
+  fs.writeFile(path.resolve(config.distDir, 'environment.js'), ' window.environment='+JSON.stringify(env)+';', function (err) {
+    if (err) {
       console.log('Error on writing env dist file\n');
       return;
     };
+
     console.log('Environment variable registered with exit. Defined as ' + env.name);
     callback();
   });
@@ -53,8 +58,8 @@ function registerEnvironment( callback ) {
 
 function registerLivereload( register ) {
   let scriptContent = register ? 'document.write(\'<script src="http://\'\n\t+ (location.host || \'localhost\').split(\':\')[0]\n\t+ \':35729/livereload.js"></\'\n\t+ \'script>\')' : '';
-  fs.writeFile( path.resolve( config.distDir, 'livereload.js' ), scriptContent, function( err ) {
-    if ( err ) {
+  fs.writeFile(path.resolve(config.distDir, 'livereload.js'), scriptContent, function (err) {
+    if (err) {
       console.log( 'Error while writing the livereload script\n' );
       return;
     };
@@ -92,18 +97,18 @@ function removeMainHash() {
   });
 }
 
-function buildRegex( urlParam ){
+function buildRegex (urlParam) {
   let basehref = envConfig.basehref || envConfig.env.basehref || null
   if ( urlParam ){
     return new RegExp("^\/" + (basehref && (basehref + "/" + urlParam)
-    || urlParam) + "\/(.*)");
+      || urlParam) + "\/(.*)");
   } else {
     return new RegExp("^" + (basehref && ("\/" + basehref) || '') + "(.*)");
   }
 };
 
-function request( host, req, res, filePath, redirect ) {
-  if ( !host && (host === "localhost" || host === "local" )) {
+function request (host, req, res, filePath, redirect) {
+  if ( !host || (host === "localhost" || host === "local" || host == "127.0.0.1" )) {
     filePath = path.resolve( config.root, filePath );
     fs.exists( filePath, ( exists ) => {
       if ( exists ) {
@@ -129,8 +134,7 @@ function request( host, req, res, filePath, redirect ) {
       });
     });
   } else {
-    // filePath = filePath.replace(new RegExp(`${config.dataDir}\/`),'');
-    let req = http.get( host + '/' + filePath, ( _res ) => {
+    let req = http.get(host+filePath, ( _res ) => {
       _res.setEncoding("utf8");
       let body = "";
       _res.on( "data", data => {
@@ -146,14 +150,14 @@ function request( host, req, res, filePath, redirect ) {
   }
 };
 
-function response( host, req, res, filePath, redirect ) {
-  if ( envConfig.middleware ) {
+function response(host, req, res, filePath, redirect) {
+  if (envConfig.middleware) {
     let middleware = require( path.resolve( __dirname, 'middlewares', envConfig.middleware));
-    middleware( req, res, ( _req, _res ) => {
-      request( host, _req, _res, filePath, redirect );
+    middleware(req, res, (_req,_res) => {
+      request(host, _req, _res, filePath, redirect);
     });
   } else {
-    request( host, req, res, filePath, redirect );
+    request(host, req, res, filePath, redirect);
   }
 }
 
@@ -169,56 +173,56 @@ function setupApp(){
   var app = express();
   app.use(bodyParser.json());
 
-  app.get('/', ( req, res ) => {
+  app.get('/', (req, res) => {
     res.setHeader('Content-Type','text/html');
-    response( envConfig.env.host, req, res, config.distDir + '/index.html', false );
+    response('', req, res, config.distDir + '/index.html', false);
   });
 
   app.get('/environment.js', ( req, res ) => {
-    res.setHeader('Content-Type','text/javascript');
-    response( envConfig.env.host, req, res, config.distDir + '/environment.js', false );
+    res.setHeader('Content-Type','application/javascript');
+    response('', req, res, config.distDir + '/environment.js', false);
   });
 
   app.get('/main.js', ( req, res ) => {
-    res.setHeader('Content-Type','text/javascript');
-    response( envConfig.env.host, req, res, config.distDir + '/main.js', false );
+    res.setHeader('Content-Type','application/javascript');
+    response('', req, res, config.distDir + '/main.js', false );
   });
 
-  app.get('/service-worker.js', ( req, res ) => {
-    res.setHeader('Content-Type','text/javascript');
-    response( envConfig.env.host, req, res, config.distDir + '/service-worker.js', false );
-  });
+  // app.get('/service-worker.js', ( req, res ) => {
+  //   res.setHeader('Content-Type','text/javascript');
+  //   response( envConfig.env.host, req, res, config.distDir + '/service-worker.js', false );
+  // });
 
-  app.get('/icons/:icon', ( req, res ) => {
-    res.setHeader('Content-Type','image/png');
-    res.sendFile( path.resolve( config.distDir, 'icons', req.params.icon ) );
-    // response( envConfig.env.host, req, res, config.distDir + '/icons/' + req.params.icon, false );
-  });
+  // app.get('/icons/:icon', ( req, res ) => {
+  //   res.setHeader('Content-Type','image/png');
+  //   res.sendFile( path.resolve( config.distDir, 'icons', req.params.icon ) );
+  //   // response( envConfig.env.host, req, res, config.distDir + '/icons/' + req.params.icon, false );
+  // });
 
-  app.post('/bulk', ( req, res ) => {
-    if ( database ) {
-      database.collection('responses').insertOne( req.body, function( err, source ) {
-        if ( err ) throw err;
-        res.sendStatus(200);
-      });
-    } else {
-      console.log( req.body );
-      res.sendStatus(200);
-    }
-  });
+  // app.post('/bulk', ( req, res ) => {
+  //   if ( database ) {
+  //     database.collection('responses').insertOne( req.body, function( err, source ) {
+  //       if ( err ) throw err;
+  //       res.sendStatus(200);
+  //     });
+  //   } else {
+  //     console.log( req.body );
+  //     res.sendStatus(200);
+  //   }
+  // });
   
   app.get(buildRegex(), ( req, res ) => {
     var redirectPath = req.params[0].replace(new RegExp(`\/${envConfig.env.basehref}\/`),'');
-    if ( !envConfig.env.host && ( envConfig.env.host != "localhost" || envConfig.env.host != "local" )) {
+    if ( !envConfig.env.host || ( envConfig.env.host != "localhost" && envConfig.env.host != "local" )) {
       if (redirectPath.indexOf(envConfig.env.apiURL) >= 0) {
         // Local API ROUTE
         redirectPath = redirectPath.replace(new RegExp(`\/${envConfig.env.apiURL}\/`),'');
-        response( envConfig.env.host, req, res, config.dataDir + '/' + redirectPath, false );
+        response(envConfig.env.host, req, res, config.dataDir + '/' + redirectPath, false);
       } else {
-        response( envConfig.env.host, req, res, config.distDir + redirectPath, !Boolean(redirectPath) || redirectPath === '/' );
+        response(envConfig.env.host, req, res, config.distDir + redirectPath, !Boolean(redirectPath) || redirectPath === '/');
       }
     } else {
-      response( envConfig.env.host, req, res, redirectPath, !Boolean(redirectPath) || redirectPath === '/' );
+      response(envConfig.env.host, req, res, redirectPath, !Boolean(redirectPath) || redirectPath === '/');
     }
   });
 
@@ -235,13 +239,13 @@ function main(){
   
   var compiler;
 
-  if ( envConfig["dev"] || !envConfig["build"] ) {
+  if (envConfig["dev"] || !envConfig["build"]) {
 
-    if ( !envConfig["dev"] ) {
+    if (!envConfig["dev"]) {
       console.warn( "Unrecognized target action. Fired development mode by default" );
-    }
+    };
 
-    function callback(){
+    function callback () {
       const app = setupApp();
       
       config.api.set("watch", true);
@@ -249,8 +253,8 @@ function main(){
 
       setupConfig();
 
-      function startApp(){
-        if (LISTENING){return}
+      function startApp () {
+        if (LISTENING) {return};
         // Serve the files on port 8000.
         server = http.createServer(app);
         
@@ -258,13 +262,14 @@ function main(){
         reload(app);
 
         // browser livereload
-        livereload(app, {watchDir: config.srcDir});
+        // livereload(app, {watchDir: config.srcDir});
 
         server.listen(app.get('port'), function () {
-          console.log('Webpack watching for changes and app listening on port 8000!\n');
+          console.log('Webpack watching for changes and app listening on port '+envConfig.port+'!\n');
           LISTENING=true;
         });
-      }
+      };
+
       compiler = webpack(config.api.get(), (err, stats) => {
         if (err) throw err;
         startApp();
@@ -273,7 +278,7 @@ function main(){
   } else if (envConfig["build"]){
     function callback(){
       
-      config.api.set( "mode", envConfig["prod"] && "production" || "development" );
+      config.api.set("mode", envConfig["prod"] && "production" || "development");
 
       setupConfig();
 
